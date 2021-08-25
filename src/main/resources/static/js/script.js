b7725b82d478d8ba8f8ace85bc30f4056822ba02
procesor2017 <31566030+procesor2017@users.noreply.github.com>
2021-08-25 12:36:04 +0200

    works on web

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,9 @@
 // append row to the HTML table
 function appendRow() {
-    var tbl = document.getElementById('myTableBody'), // table reference
-        row = tbl.insertRow(tbl.rows.length),      // append table row
+    var tbl = document.getElementById('myTableBody'), 
+        row = tbl.insertRow(tbl.rows.length),     
         i;
-    // insert table cells to the new row
+
     for (i = 0; i < tbl.rows[0].cells.length; i++) {
         createCell(row.insertCell(i));
     }
@@ -11,21 +11,20 @@ function appendRow() {
  
 // create DIV element and append to the table cell
 function createCell(cell) {
-    var div = document.createElement('input'); // create DIV element
+    var div = document.createElement('input'); 
     cell.appendChild(div)
 }
 
 // append column to the HTML table
 function appendColumn() {
-    var tbl = document.getElementById('myTableBody'), // table reference
+    var tbl = document.getElementById('myTableBody'), 
         i;
     var theadRow = document.getElementById("myTableTheadRow");
     var numberCell = theadRow.cells.length;
     var cel = theadRow.insertCell(numberCell);
     cel.innerHTML = String(numberCell);
     
-    
-    // open loop for each row and append cell
+
     for (i = 0; i < tbl.rows.length; i++) {
 
         createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length));
@@ -35,8 +34,8 @@ function appendColumn() {
 
 // delete table rows with index greater then 0
 function deleteRows() {
-    var tbl = document.getElementById('myTableBody'), // table reference
-        lastRow = tbl.rows.length - 1,             // set the last row index
+    var tbl = document.getElementById('myTableBody'), 
+        lastRow = tbl.rows.length - 1,             
         i;
     // delete rows with index greater then 0
     for (i = lastRow; i > 0; i--) {
@@ -46,8 +45,8 @@ function deleteRows() {
  
 // delete table columns with index greater then 0
 function deleteColumns() {
-    var tbl = document.getElementById('myTableBody'), // table reference
-        lastCol = tbl.rows[0].cells.length - 1,    // set the last column index
+    var tbl = document.getElementById('myTableBody'), 
+        lastCol = tbl.rows[0].cells.length - 1,    
         i, j;
     // delete cells with index greater then 0 (for each row)
     for (i = 0; i < tbl.rows.length; i++) {
@@ -64,34 +63,18 @@ function sendData() {
     for (i = 0; i < tbody.rows.length; i++) {
         var dict = {}
         for (j = 0; j < tbody.rows[0].cells.length; j++){
-            dict[j] = tbody.rows[i].cells[j].childNodes[0].value;
+            if (tbody.rows[i].cells[j].childNodes[0].value != ""){
+                dict[j] = tbody.rows[i].cells[j].childNodes[0].value;
+            }
         }
-        console.log(dict)
-        myRows.push(dict)
+        if (Object.keys(dict).length !== 0){
+            myRows.push(dict);
+        }
     }
-    console.log("=== Final ===")
-    console.log(myRows)
-    document.getElementById("Response").value = myRows;
-
-
 
     const myJSON = JSON.stringify(myRows);
-
     document.getElementById("Response").value = myJSON;
-    /*
-    var obj = []
 
-    for (i = 0; i < myRows.length; i++){
-        var catchArr = []
-        for (j = 0; j < myRows[i][0].length; j++){
-            catchArr.push(myRows[i]);
-            console.log("============= pokus o catch arr ====");
-            console.log(catchArr[0][0][j]);
-        }
-    }
-    
-    const myJSON = JSON.stringify(obj);
-    
-    document.getElementById("Response").value = myJSON;*/
+
     
 }
