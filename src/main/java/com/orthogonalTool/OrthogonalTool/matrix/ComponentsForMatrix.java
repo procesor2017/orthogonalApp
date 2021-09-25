fc656bd023732d5114f0c71b0797d29e43b266a3
Jan Egermaier <62232822+janegermaier@users.noreply.github.com>
2021-09-25 14:22:18 +0200

    change itetation for loop

--- src/main/java/com/orthogonalTool/OrthogonalTool/matrix/ComponentsForMatrix.java
+++ src/main/java/com/orthogonalTool/OrthogonalTool/matrix/ComponentsForMatrix.java
@@ -27,14 +27,9 @@ public class ComponentsForMatrix {
     }
 
     public String[][] iterDatatableFromUser(String[][] dataTable){
-        /*
-        Každý sloupeček bude mít ID 0-n a hodnotu size() pak se mi iterizací budou řadit podle velikosti
-        */
-
-
         HashMap<Integer, Integer> my_dict = new HashMap<Integer, Integer>();
         String[][] newTable = new String[dataTable.length][dataTable[0].length];
-        //Mapa pro iterizované hodnoty
+        //Maps for iterating
         LinkedHashMap<Integer, Integer> sortedMap = new LinkedHashMap<>();
 
         for (int i = 0; i < dataTable[0].length; i++){
@@ -62,29 +57,17 @@ public class ComponentsForMatrix {
         Set<Integer> keys = sortedMap.keySet();
         int k = 0;
         for (Integer key : keys) {
-            colArrayValue[k] = key;   //ukládá pořadí sloupcu jak se mají doplnovat do nové tabulky
+            colArrayValue[k] = key;
             rowArrayValue[k] = sortedMap.get(key);
             k++;
         }
-        System.out.println(Arrays.toString(colArrayValue));
 
-
-        System.out.println("New table has row: " + newTable.length + "and col:" + newTable[0].length);
-
-
-        //2d array = první řádek pak coll
-        //coll
         for (int i = 0; i < sortedMap.size(); i++){
-            //row
             for(int j = 0; j < rowArrayValue[i]; j++){
-              //přeskládat otDatatable tak aby odpovídali col a řádkum z my_dict tedy třeba col 2 se přesune na první pozici a první col na poslední
-                System.out.println("value of array : " + rowArrayValue[i]);
-                System.out.println("datatable[j][colarrayvalue[j]]: " + dataTable[j][colArrayValue[j]]);
                 newTable[j][i] = dataTable[j][colArrayValue[i]];
             }
         }
-        System.out.println(Arrays.deepToString(dataTable));
-        System.out.println(Arrays.deepToString(newTable));
+
         return newTable;
     }
 }
